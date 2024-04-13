46fb8ca91f34ad56e9444ba16c922d5e756936cf
hossein <hosseinbakhtiari781@outlook.com>
2024-04-13 22:35:36 +0330

    create the card component and styled that and create the helper file for the shorten the titles of products

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useProducts } from "../context/ProductContext";
-
-import styles  from "./ProductsPage.module.css";
+import Card from "../components/Card";
+import styles from "./ProductsPage.module.css";
 function ProductsPage() {
   const products = useProducts();
   console.log(products);
@@ -9,8 +9,8 @@ function ProductsPage() {
     <div className={styles.container}>
       <div className={styles.products}>
         {!products.length && <p>Loading...</p>}
-        {products.map((product) => (
-          <p key={product.id}>{product.title}</p>
+        {products.map((p) => (
+          <Card key={p.id} data={p} />
         ))}
       </div>
       <div>sidebar</div>
