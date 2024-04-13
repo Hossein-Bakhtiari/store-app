9df9f7f97826a383f8579386b919166780456fee
hossein <hosseinbakhtiari781@outlook.com>
2024-04-13 19:52:47 +0330

    set routing process

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { Navigate, Route , Routes} from "react-router-dom";
+import ProductsPage from "./pages/ProductsPage";
+import DetailsPage from "./pages/DetailsPage";
+import CheckingoutPage from "./pages/CheckingoutPage";
+import NotFoundPage from "./pages/404"
 function App() {
   return (
-    <>
-      <h1>ShoppingApp</h1>
-    </>
+      <Routes>
+        <Route path="/" element={<Navigate to="/products" replace/>}/>
+        <Route path="/products" element={<ProductsPage />}/>
+        <Route path="/products/:id" element={<DetailsPage />}/>
+        <Route path="/checkout" element={<CheckingoutPage />}/>
+        <Route path="/*" element={<NotFoundPage />}/>
+      </Routes> 
   );
 }
 
