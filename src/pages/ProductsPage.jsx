4dff38b8efae50ce029374208be33526bc5ec42e
hossein <hosseinbakhtiari781@outlook.com>
2024-04-14 23:27:51 +0330

    add the search section

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,21 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { useProducts } from "../context/ProductContext";
+
 import Card from "../components/Card";
 import styles from "./ProductsPage.module.css";
 import Loader from "../components/Loader";
+import { ImSearch } from "react-icons/im";
+
 function ProductsPage() {
+  const [search, setSearch] = useState("");
   const products = useProducts();
   console.log(products);
+
+  const searchHndler = () => {
+    console.log("search");
+  };
+
   return (
-    <div className={styles.container}>
-      <div className={styles.products}>
-        {!products.length && <Loader />}
-        {products.map((p) => (
-          <Card key={p.id} data={p} />
-        ))}
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase().trim())}
+        />
+        <button onClick={searchHndler}>
+          <ImSearch />
+        </button>
       </div>
-      <div>sidebar</div>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {!products.length && <Loader />}
+          {products.map((p) => (
+            <Card key={p.id} data={p} />
+          ))}
+        </div>
+        <div>sidebar</div>
+      </div>
+    </>
   );
 }
 
