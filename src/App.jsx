c17c2cae44bbf6429da57ddd95e1de6f89b52d23
hossein <hosseinbakhtiari781@outlook.com>
2024-04-18 15:41:07 +0330

    create CartContext and useReducer hook  for the managing data in Cart

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,20 @@ import DetailsPage from "./pages/DetailsPage";
 import CheckingoutPage from "./pages/CheckingoutPage";
 import NotFoundPage from "./pages/404";
 import ProductProvider from "./context/ProductContext";
+import CartProvider from "./context/CartContext";
 function App() {
   return (
-    <ProductProvider>
-      <Routes>
-        <Route path="/" element={<Navigate to="/products" replace />} />
-        <Route path="/products" element={<ProductsPage />} />
-        <Route path="/products/:id" element={<DetailsPage />} />
-        <Route path="/checkout" element={<CheckingoutPage />} />
-        <Route path="/*" element={<NotFoundPage />} />
-      </Routes>
-    </ProductProvider>
+    <CartProvider>
+      <ProductProvider>
+        <Routes>
+          <Route path="/" element={<Navigate to="/products" replace />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/products/:id" element={<DetailsPage />} />
+          <Route path="/checkout" element={<CheckingoutPage />} />
+          <Route path="/*" element={<NotFoundPage />} />
+        </Routes>
+      </ProductProvider>
+    </CartProvider>
   );
 }
 
